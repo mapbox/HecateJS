2def5b6ef221ed34b41ed2ea1bfb1918b15f96ab
ingalls <ingalls@protonmail.com>
2018-07-17 11:25:22 -0400

    Add lib/auth tests

---
+++ test/lib.auth.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Hecate = require('../cli.js');
+
+const test = require('tape').test;
+const nock = require('nock');
+
+test('Assert obtaining delta according to ID', (t) => {
+    nock('http://localhost:7777')
+        .get('/api/auth')
+        .reply(200, {
+            custom: 'auth'
+        });
+
+    const hecate = new Hecate({
+        host: 'localhost',
+        port: '7777'
+    });
+
+    hecate.auth(null, (err, res) => {
+        t.error(err, 'no error');
+
+        t.deepEquals(res, { custom: 'auth' }, 'returns custom auth json');
+        t.end();
+    });
+});
