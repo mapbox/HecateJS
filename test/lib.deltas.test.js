3f9f0ae3861f743b50067ba1a2db3d30eea33e10
Lizzie Gooding <lizzie.gooding@mapbox.com>
2019-11-05 20:27:11 -0500

    support offset parameter for listDeltas

--- test/lib.deltas.test.js
+++ test/lib.deltas.test.js
@@ -6,13 +6,13 @@ const test = require('tape').test;
 const nock = require('nock');
 
 test('lib.deltas.test.js', (t) => {
-    nock('http://localhost:7777')
-        .get('/api/deltas?limit=100')
-        .reply(200, true);
-
     const hecate = new Hecate({
         url: 'http://localhost:7777'
     });
+
+    nock('http://localhost:7777')
+        .get('/api/deltas?limit=100')
+        .reply(200, true);
 
     t.test('lib.deltas.test.js - default', (q) => {
         hecate.listDeltas({}, (err, res) => {
@@ -26,9 +26,24 @@ test('lib.deltas.test.js', (t) => {
         .get('/api/deltas?limit=1')
         .reply(200, true);
 
-    t.test('lib.deltas.test.js - default', (q) => {
+    t.test('lib.deltas.test.js - custom limit', (q) => {
         hecate.listDeltas({
             limit: 1
+        }, (err, res) => {
+            q.error(err, 'no errors');
+            q.equals(res, true);
+            q.end();
+        });
+    });
+
+    nock('http://localhost:7777')
+        .get('/api/deltas?limit=1&offset=1')
+        .reply(200, true);
+
+    t.test('lib.deltas.test.js - custom offset', (q) => {
+        hecate.listDeltas({
+            limit: 1,
+            offset: 1
         }, (err, res) => {
             q.error(err, 'no errors');
             q.equals(res, true);
