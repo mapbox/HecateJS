26c313bc244c8bf2baddf382b7edbf8def7b0628
ingalls <ingalls@protonmail.com>
2019-11-22 10:17:05 -0400

    Finish validateGeoJSON tests

--- test/util.validateGeojson.test.js
+++ test/util.validateGeojson.test.js
@@ -1,11 +1,18 @@
 'use strict';
 
 const fs = require('fs');
+const Ajv = require('ajv');
 const path = require('path');
 const tape = require('tape');
 const validateGeojson = require('../util/validateGeojson.js');
 const pipeline = require('stream').pipeline;
 const split = require('split');
+
+const ajv = new Ajv({
+        schemaId: 'auto'
+});
+
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
 
 // Assert the errors in the geojson file
 tape('Assert fails', (t) => {
@@ -115,62 +122,29 @@ tape('Assert valid features', (t) => {
     );
 });
 
-/**
-tape.only('Assert fails according to schema ', (t) => {
-    const pathName = path.resolve(__dirname, '.', './fixtures/invalid-geojson-schema.json');
-    const schema = fs.readFileSync(path.resolve(__dirname, '.', './fixtures/schema.json'), 'utf8');
-    let message;
-    let error;
-    let description;
-    let linenumber;
+tape('Assert fails according to schema ', (t) => {
+    const schemaJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.', './fixtures/schema.json'), 'utf8'));
 
-    // Validate the corrupted sample data
-    const geojsonErrs = validateGeojson(pathName, { schema: JSON.parse(schema) });
-    t.ok(geojsonErrs.length > 0, true, 'file is not a valid geoJSON file');
+    const schema = ajv.compile(schemaJson);
 
-    for (let item in geojsonErrs) {
-        message = JSON.parse(geojsonErrs[item]);
-        item = parseInt(item);
-
-        switch (item) {
-            case 0:
-                linenumber = 1;
-                error = '[{"message":"should have required property \'source\'"}]';
-                description = 'should have required property \'source\'';
-                break;
-            case 1:
-                linenumber = 2;
-                error = '[{"message":"should have required property \'street\'"}]';
-                description = 'should have required property \'street\'';
-                break;
-            case 2:
-                linenumber = 3;
-                error = '[{"message":"should have required property \'number\'"}]';
-                description = 'should have required property \'number\'';
-                break;
-            case 3:
-                linenumber = 4;
-                error = '[{"message":"should be equal to one of the allowed values"}]';
-                description = 'prop1 should be equal to one of the allowed values';
-                break;
-            case 4:
-                linenumber = 5;
-                error = '[{"message":"should be string,number"}]';
-                description = 'postcode should be string,number';
-                break;
-            case 5:
-                linenumber = 6;
-                error = '[{"message":"should be array"}]';
-                description = 'street should be array';
-                break;
+    t.deepEquals(validateGeojson.validateFeature({
+        'properties': {
+            'number':0,
+            'street':[{ 'display':'\\N','priority':0 }]
+        },
+        'geometry':{
+            'type':'Point',
+            'coordinates': [23.6,23.5]
         }
+    }, {
+        schema: schema
+    }), [{
+        message: 'should have required property \'source\'',
+        linenumber: 0
+    },{
+        message: '"type" member required',
+        linenumber: 0
+    }]);
 
-        if (linenumber) {
-            t.equals(message.linenumber, linenumber, `the line number is ${linenumber}`);
-            t.equals(JSON.stringify(message.error), error, description);
-        }
-    }
     t.end();
 });
-
-*/
