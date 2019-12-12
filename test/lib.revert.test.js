bb5f5cf2d74b746bbb4e87328b12cb24d79164fb
ingalls <ingalls@protonmail.com>
2019-12-12 15:18:40 -0400

    Test reversion accross deltas

--- test/lib.revert.test.js
+++ test/lib.revert.test.js
@@ -4,6 +4,7 @@ const Hecate = require('../cli.js');
 
 const test = require('tape');
 const nock = require('nock');
+const PassThrough = require('stream').PassThrough;
 
 test('Clean revert of single delta', (t) => {
     const hecate = new Hecate({
@@ -57,17 +58,176 @@ test('Clean revert of single delta', (t) => {
                 }
             }]);
 
+    let buffer = '';
+    const results = new PassThrough().on('data', (data) => {
+        buffer += String(data);
+    }).on('end', () => {
+        buffer = buffer.trim().split('\n').map(buff => JSON.parse(buff));
+
+        t.deepEquals(buffer, [{
+            id: 1,
+            type: 'Feature',
+            action: 'modify',
+            version: 2,
+            properties: {
+                created: true
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [ 1, 1 ]
+            }
+        }]);
+        t.end();
+    });
+
     hecate._.revert.deltas({
+        output: results,
         start: 2,
         end: 2
     }, (err, res) => {
         t.error(err);
-        t.end();
     });
 });
 
 test('Clean revert of multiple deltas', (t) => {
-    t.end();
+    const hecate = new Hecate({
+        url: 'http://localhost:7777'
+    });
+
+    nock('http://localhost:7777')
+        .get('/api/delta/2')
+            .reply(200, {
+                features: {
+                    type: 'FeatureCollection',
+                    features: [{
+                        id: 1,
+                        version: 2,
+                        action: 'modify'
+                    }]
+                }
+            })
+        .get('/api/delta/3')
+            .reply(200, {
+                features: {
+                    type: 'FeatureCollection',
+                    features: [{
+                        id: 2,
+                        version: 3,
+                        action: 'delete'
+                    }]
+                }
+            })
+        .get('/api/data/feature/1/history')
+            .reply(200, [{
+                feat: {
+                    id: 1,
+                    type: 'Feature',
+                    action: 'modify',
+                    version: 2,
+                    properties: {
+                        modified: true
+                    },
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [ 0.0, 0.0 ]
+                    }
+                }
+            },{
+                feat: {
+                    id: 1,
+                    type: 'Feature',
+                    action: 'create',
+                    version: 1,
+                    properties: {
+                        created: true
+                    },
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [ 1.0, 1.0 ]
+                    }
+                }
+            }])
+        .get('/api/data/feature/2/history')
+            .reply(200, [{
+                feat: {
+                    id: 2,
+                    type: 'Feature',
+                    action: 'delete',
+                    version: 3,
+                    properties: null,
+                    geometry: null
+                }
+            },{
+                feat: {
+                    id: 2,
+                    type: 'Feature',
+                    action: 'modify',
+                    version: 2,
+                    properties: {
+                        modified: true
+                    },
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [ 0.0, 0.0 ]
+                    }
+                }
+            },{
+                feat: {
+                    id: 2,
+                    type: 'Feature',
+                    action: 'create',
+                    version: 1,
+                    properties: {
+                        created: true
+                    },
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [ 1.0, 1.0 ]
+                    }
+                }
+            }]);
+
+    let buffer = '';
+    const results = new PassThrough().on('data', (data) => {
+        buffer += String(data);
+    }).on('end', () => {
+        buffer = buffer.trim().split('\n').map(buff => JSON.parse(buff));
+
+        t.deepEquals(buffer, [{
+            id: 1,
+            type: 'Feature',
+            action: 'modify',
+            version: 2,
+            properties: {
+                created: true
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [ 1, 1 ]
+            }
+        },{
+            id: 2,
+            type: 'Feature',
+            action: 'restore',
+            version: 3,
+            properties: {
+                modified: true
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [ 0, 0 ]
+            }
+        }]);
+        t.end();
+    });
+
+    hecate._.revert.deltas({
+        output: results,
+        start: 2,
+        end: 3
+    }, (err, res) => {
+        t.error(err);
+    });
 });
 
 test('Failed revert as feature exists multiple times accross detlas', (t) => {
